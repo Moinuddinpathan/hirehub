b43cf638ff63cfaf9222243216de035f3ac693bc
Moinuddin Pathan <moinuddinpathan996@gmail.com>
2026-09-11 17:51:26 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,9 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }));
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -75,6 +75,6 @@ app.use("/api/admin", adminRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on Port ${PORT}`);
 });
