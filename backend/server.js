91b8ac30d6f9869f3d19e6b131a3f32de9412bb4
Moinuddin Pathan <moinuddinpathan996@gmail.com>
2026-09-12 13:19:08 +0530

    Fix production frontend URL

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,13 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
