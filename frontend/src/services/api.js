659fa3531f006f84f918958863f378ecf2751b0d
Moinuddin Pathan <moinuddinpathan996@gmail.com>
2026-09-13 15:48:34 +0530

    Fix API configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
-  if(token) {
+  if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
 
-    return req;
-
-})
+  return req;
+});
 
 export default api;
