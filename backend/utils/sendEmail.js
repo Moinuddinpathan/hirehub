2447757d62248096340950c1b39157c88c298a70
Moinuddin Pathan <moinuddinpathan996@gmail.com>
2026-09-14 12:28:08 +0530

    Fix Gmail SMTP connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,13 +2,20 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("EMAIL SMTP ERROR:", error);
+  } else {
+    console.log("EMAIL SERVER IS READY");
+  }
 });
 
 const sendOtpEmail = async (email, otp) => {
